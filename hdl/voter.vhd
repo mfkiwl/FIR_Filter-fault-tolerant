49dde6c1dff8867983b1c068a968b77500fa2033
miloshunter <milostravian2@gmail.com>
2019-07-11 03:00:43 +0200

    Improved voter

--- hdl/voter.vhd
+++ hdl/voter.vhd
@@ -20,22 +20,10 @@ architecture Behavioral of voter is
   signal s_majority : std_logic_vector(N-1 downto 0);
 begin
 
-  o_majority <= s_majority;
 
-  vote : process (i_u1, i_u2, i_u3, i_u4, i_u5) is
-  begin  -- process vote
-    if to_integer(signed((i_u1 and i_u2 and i_u3) or (i_u1 and i_u2 and i_u4) or
-                         (i_u1 and i_u2 and i_u5) or (i_u1 and i_u3 and i_u4) or
-                         (i_u1 and i_u4 and i_u5))) = to_integer(signed(i_u1)) then
-      s_majority <= i_u1;
-    elsif to_integer(signed((i_u2 and i_u3 and i_u4) or (i_u2 and i_u3 and i_u5) or
-                            (i_u2 and i_u4 and i_u5))) = to_integer(signed(i_u2)) then
-      s_majority <= i_u2;
-    elsif to_integer(signed((i_u3 and i_u4 and i_u5))) = to_integer(signed(i_u3)) then
-      s_majority <= i_u3;
-    else
-      s_majority <= i_u1;
-    end if;
-  end process vote;
+  o_majority <= ((i_u1 and i_u2 and i_u3) or (i_u1 and i_u2 and i_u4) or (i_u1 and i_u3 and i_u4) or
+                 (i_u2 and i_u3 and i_u4) or (i_u1 and i_u2 and i_u5) or (i_u1 and i_u4 and i_u5) or
+                 (i_u1 and i_u3 and i_u5) or (i_u2 and i_u3 and i_u5) or (i_u3 and i_u4 and i_u5) or
+                 (i_u2 and i_u4 and i_u5));
 
 end Behavioral;
